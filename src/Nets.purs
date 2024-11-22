b2003a03c4b16fba9840f7edb67dc953735fa264
Alex Kuczala <kookzala@gmail.com>
2024-11-21 23:33:25 -0500

    fix redex isomorphism

--- src/Nets.purs
+++ src/Nets.purs
@@ -30,7 +30,7 @@ module Nets
 import Prelude
 
 import Control.Apply (lift2)
-import Control.Monad.State (class MonadState, State, evalState, execState, gets, modify, modify_)
+import Control.Monad.State (class MonadState, State, evalState, execState, get, gets, modify, modify_, put, runState)
 import Data.Array (all, filter, zipWithA)
 import Data.Array as A
 import Data.Bifunctor (bimap)
@@ -266,7 +266,10 @@ instance Isomorphic Tree where
           pure true
         _ -> pure false
 
-    lookup :: VarLabel -> VarLabel -> State (Bimap VarLabel) (Tuple (Maybe VarLabel) (Maybe VarLabel))
+    lookup
+      :: VarLabel
+      -> VarLabel
+      -> State (Bimap VarLabel) (Tuple (Maybe VarLabel) (Maybe VarLabel))
     lookup s1 s2 = gets $ bimap (M.lookup s1) (M.lookup s2)
 
 flipRedex :: Redex -> Redex
@@ -275,16 +278,19 @@ flipRedex (Redex x y) = (Redex y x)
 sortRedex :: Redex -> Redex
 sortRedex r@(Redex x y) = if x < y then r else flipRedex r
 
--- TODO: how to handle (A ~ B) <-> (B ~ A) isomorphism?
--- could we mess up the state by crawling the wrong pair?
--- does this work??
+-- Given (A ~ B), (C ~ D), first check tree isomorphisms A <-> C & B <-> D
+-- then try A <-> D and B <-> C
 instance Isomorphic Redex where
-  isomorphic r1 r2 =
-    let
-      (Redex x1 y1) = sortRedex r1
-      (Redex x2 y2) = sortRedex r2
-    in
-      lift2 (&&) (isomorphic x1 x2) (isomorphic y1 y2)
+  isomorphic r1 r2 = do
+    varMap <- get
+    let Tuple isIso s = runState (liftIso r1 r2) varMap
+    if isIso then do
+      put s
+      pure true
+    else
+      liftIso r1 (flipRedex r2)
+    where
+    liftIso (Redex x1 y1) (Redex x2 y2) = (lift2 (&&) (isomorphic x1 x2) (isomorphic y1 y2))
 
 _varGenState :: forall a r. Lens' { varGenState :: a | r } a
 _varGenState = prop (Proxy :: Proxy "varGenState")
